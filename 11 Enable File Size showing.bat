a39d4192aea70ea74fcd583578c6ae8e7d307eed
Dineth Pansilu <dinethpansilu2004@gmail.com>
2025-05-19 13:02:58 +0530

    update

--- 11 Enable File Size showing.bat
+++
@@ -1,4 +0,0 @@
-@echo off
-reg add "HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced" /v "ShowCompColor" /t REG_DWORD /d "1" /f
-taskkill /f /im explorer.exe
-start explorer.exe
